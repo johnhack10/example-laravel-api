29558bbe6130d00dff4b25233c3d4431626bcd40
John Arango <jfamdevelop@gmail.com>
2024-11-14 21:39:35 -0500

    Initial commit of Laravel Api project

---
+++ app/Http/Resources/V2/PostCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\V2;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Platzi',
+                'authors' => [
+                    'Chucho',
+                    'Tarzan'
+                ]
+                ],
+                'type' => 'articles'
+            ];
+    }
+}
